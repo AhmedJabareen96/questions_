78055ebd1bdd1fd2dcfdbfd8a97442e4a53a2261
Ahmed Jabareen <ahmed.jabareen1@gmail.com>
2023-04-22 16:09:35 +0300

    added 1.3

---
+++ Question1.3.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function (myArr) {
+    var index = 0;
+    while (index < myArr.length) {
+        if (myArr[index] === myArr[index + 1])
+            myArr.splice(index + 1, 1);
+        else
+            index++;
+    }
+    console.log(myArr);
+};
+removeDuplicates([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]);
---
+++ Question1.3.ts
@@ -0,0 +1,12 @@
+const removeDuplicates = (myArr:number[]) : void => {
+    let index = 0;
+    while(index < myArr.length){
+        if(myArr[index] === myArr[index+1])
+            myArr.splice(index+1, 1);
+        else
+            index++;
+    }
+    console.log(myArr);
+};
+
+removeDuplicates([1,2,2,3,3,3,4,4,4,4,5,5,5,5,5])
